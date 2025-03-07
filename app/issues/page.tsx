e5addd01e2cd35ef6a247164ee99a7cce10442d1
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-03-07 14:48:20 +0530

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Button } from "@radix-ui/themes";
 const IssuesPage = () => {
-  return <div>hola</div>;
+  return (
+    <>
+      <Button>New Issue</Button>
+    </>
+  );
 };
 
 export default IssuesPage;
