041390addf4bf6cce053ee6d541cd2111ebd081d
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-03-11 13:16:35 +0530

    Refactor and create edit issues page

---
+++ app/components/IssueForm.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { issue } from "@prisma/client";
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+const IssueForm = ({ issue }: { issue?: issue }) => {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occurred.");
+    }
+  });
+
+  return (
+    <div className=" space-y-6 max-w-xl ">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form onSubmit={onSubmit}>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <TextField.Root
+          className="mb-2"
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
---
+++ app/issues/[id]/EditIssue.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import IssueForm from "@/app/components/IssueForm";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+
+const EditIssue = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssue;
