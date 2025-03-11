fffd41ec5fcd0364c184f290ae0d3a1bd4eae86b
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-03-11 19:36:54 +0530

    Build delete issue button and api request

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button, AlertDialog, Flex } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  const router = useRouter();
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              color="red"
+              onClick={async () => {
+                await axios.delete("/api/issues/" + issueId);
+                router.push("/issues");
+                router.refresh();
+              }}
+            >
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
+
+export default DeleteIssueButton;
