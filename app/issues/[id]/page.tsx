1e9199ec081e1462ced111a6d12dbcbf9a716445
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-03-11 12:42:27 +0530

    Add markdown and additional skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { notFound } from "next/navigation";
-import React from "react";
+import ReactMarkdown from "react-markdown";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 
 interface Props {
@@ -21,9 +21,8 @@ const IssueDetailsPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        {" "}
-        <p>{issue.description}</p>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
