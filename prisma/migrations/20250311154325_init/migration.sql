b9436326b9daa3e40b49f515eeb98b8224f9be62
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-04-03 18:50:05 +0530

    Add assigned issues to prisma schema, use API to assign issues

--- prisma/migrations/20250311154325_init/migration.sql
+++ prisma/migrations/20250311154325_init/migration.sql
@@ -6,7 +6,6 @@ CREATE TABLE `issue` (
     `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -62,8 +61,8 @@ CREATE TABLE `VerificationToken` (
     UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- AddForeignKey
-ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- AlterTable
+ ALTER TABLE `Issue` ADD COLUMN `assignedToUserId` VARCHAR(255) NULL;
+ 
+ -- AddForeignKey
+ ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedToUserId_fkey` FOREIGN KEY (`assignedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
