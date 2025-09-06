87fc6181f3ababf465bed14d75444d1855eca76f
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-09-06 12:46:10 +0530

    Fix SSR errors

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
-import dynamic from "next/dynamic";
-import IssueFormSkeleton from "./loading";
-
-const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
-  ssr: false,
-  loading: () => <IssueFormSkeleton />,
-});
+import IssueFormClient from "../../_components/IssueFormClient";
 
 interface Props {
   params: { id: string };
@@ -20,7 +14,7 @@ const EditIssuePage = async ({ params }: Props) => {
 
   if (!issue) notFound();
 
-  return <IssueForm issue={issue} />;
+  return <IssueFormClient issue={issue} />;
 };
 
 export default EditIssuePage;
